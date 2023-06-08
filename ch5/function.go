e0273b66d4aa3de2cf3e8b7e2a35de2134b7df25
emptyurmind <2390757768@qq.com>
2023-06-08 23:58:22 +0800

    update function.go

--- ch5/function.go
+++ ch5/function.go
@@ -1,6 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+// prereqs记录了每个课程的前置课程
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
 
 func main() {
 	// 函数值不仅仅是一串代码，还记录了状态
@@ -11,6 +32,12 @@ func main() {
 	fmt.Println(f()) // "9"
 	fmt.Println(f()) // "16"
 	// 变量的生命周期不由它的作用域决定：squares返回后，变量x仍然隐式的存在于f中。
+
+	fmt.Println("---------------")
+
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
 }
 
 func squares() func() int {
@@ -22,3 +49,25 @@ func squares() func() int {
 }
 func negative(n int) int   { return -n }
 func product(m, n int) int { return m * n }
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	visitAll(keys)
+	return order
+}

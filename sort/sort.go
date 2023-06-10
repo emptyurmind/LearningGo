9ca57e668474d8de5e5efaea9948f79fb4b56129
emptyurmind <2390757768@qq.com>
2023-06-10 21:11:45 +0800

    add countingsort to sort.go

--- sort/sort.go
+++ sort/sort.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -13,6 +14,7 @@ func main() {
 	fmt.Println(mergeSort(arr))
 	fmt.Println(quickSort(arr))
 	fmt.Println(heapSort(arr))
+	fmt.Println(countingSort(arr))
 }
 
 // 冒泡排序
@@ -199,4 +201,37 @@ func heapify(arr []int, i, arrLen int) {
 
 func swap(arr []int, i, j int) {
 	arr[i], arr[j] = arr[j], arr[i]
+}
+
+func countingSort(arr []int) []int {
+	// （1）找出待排序的数组中最大和最小的元素
+	// （2）统计数组中每个值为i的元素出现的次数，存入数组C的第i项
+	// （3）对所有的计数累加（从C中的第一个元素开始，每一项和前一项相加）
+	// （4）反向填充目标数组：将每个元素i放在新数组的第C(i)项，每放一个元素就将C(i)减去1
+	max := math.MinInt
+	for _, value := range arr {
+		if max < value {
+			fmt.Printf("i's value is %d\n", value)
+			max = value
+		}
+	}
+	fmt.Printf("max's value is %d\n", max)
+	bucketLen := max + 1
+	bucket := make([]int, bucketLen) // 初始为0的数组
+
+	sortedIndex := 0
+
+	for i := range arr {
+		bucket[arr[i]] += 1
+	}
+
+	for j := 0; j < bucketLen; j++ {
+		for bucket[j] > 0 {
+			arr[sortedIndex] = j
+			sortedIndex += 1
+			bucket[j] -= 1
+		}
+	}
+
+	return arr
 }
